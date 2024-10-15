ee63305c19103ce1759ce6575e0bc594b0accc84
bernardolv <19933581+bernardolv@users.noreply.github.com>
2024-10-15 16:21:38 -0600

    Added ZenkoGenerator initial implenetation

---
+++ ZenkoGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Zenko;
+using Zenko.Entities;
+using Zenko.Services;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("HI");
+        LevelSettings levelSettings = new LevelSettings();
+        levelSettings.outerDimension = 6;
+        levelSettings.innerDimension = 4;
+        levelSettings.wallsAmount = 2;
+
+        MapGeneratorService generatorService = new MapGeneratorService(levelSettings, 1000);
+
+        Func<Map, int, bool> callback = new Func<Map, int, bool>((Map map, int turns) =>
+        {
+
+            foreach (string line in MapService.ConvertToStringArray(map))
+            {
+                Logger.Log(line);
+            }
+
+            return false;
+        });
+
+        generatorService.SetOnMapFoundCallBack(callback);
+
+        generatorService.Generate();
+
+    }
+}
